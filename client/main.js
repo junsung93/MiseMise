3501a897db1d65e4efa7bdb39b39011a324d3c76
Junsung <fch21@naver.com>
2016-11-10 00:44:42 +0900

    css

---
+++ client/main.js
@@ -0,0 +1,12 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+Template.hello.helpers({
+
+});
+
+Template.hello.events({
+
+});
