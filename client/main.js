6ff720e588cdecf80ae0f5ebdaadc9957e3fd435
Junsung <fch21@naver.com>
2016-11-22 22:27:52 +0900

    하단 바 클릭시 이벤트 발생

--- client/main.js
+++ client/main.js
@@ -1,12 +1,20 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import {Template} from 'meteor/templating';
+import {ReactiveVar} from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.helpers({
-
-});
-
-Template.hello.events({
-
+Template.underBar.events({
+    'click #underBar': function () {
+        if(document.getElementById('underBar').style.height == "430px") {
+            document.getElementById('underBar').style.height = "1330px";
+            document.getElementById('emo').style.height = 0;
+            document.getElementById('emo').style.width = 0;
+        }
+        else {
+            document.getElementById('underBar').style.height = "430px";
+            document.getElementById('emo').style.height = "500px";
+            document.getElementById('emo').style.width = "500px";
+        }
+        console.log("click");
+    }
 });
